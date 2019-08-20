2a5ca978d5e71d3e1cd900cf0073f5e716b963ec
Owen Pembery <o.r.pembery@bath.ac.uk>
2019-08-20 14:25:29 +0100

    Things updated with (I hope) final nbpc qmc results.

--- copy-nbpc-data.sh
+++ copy-nbpc-data.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-scp -r orp20@linux.bath.ac.uk:/mnt/orp20-XDrive/Maths/ResearchProjects/EASpence/RC-MA1200/owen/data/data* data/
+scp -r orp20@linux.bath.ac.uk:/mnt/orp20-XDrive/Maths/ResearchProjects/EASpence/RC-MA1200/owen/data/data-for-nb* data/
 
