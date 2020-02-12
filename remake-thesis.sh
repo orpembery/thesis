0b7bbac441abbe85076a707279feb390af036d19
Owen Pembery <o.r.pembery@bath.ac.uk>
2020-02-12 22:05:51 +0000

    Now have shell script to remake all plots.

---
+++ remake-thesis.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+touch calc_G.py
+touch interpolation-figure.py
+touch nbpc-qmc-parallel-table.py
+touch plot-G.py
+touch quick-gmres-analysis.py
+touch calculate-qmc-error.py
+touch l1-plot-and-table.py
+touch nbpc-qmc-sequential-table.py
+touch plot-probabalistic-runs.py
+touch sample-grid.py
+touch gmres-blow-up-plot.py
+touch n_actual.py
+touch petsc-tolerances.py
+touch pollution-figure.py
+touch test_calc_g.py
+# gmres-blow-up.py # This script is excluded, because it performs many finite-element calculations
+touch nbpc-linfinity-plots.py
+touch pic_calc.py
+touch prob-gmres-theory-plots.py
+
+make
