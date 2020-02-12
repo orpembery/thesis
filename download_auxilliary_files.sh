e7684d8d07565b34eac1e919cb27bd251f4164f2
Owen Pembery <o.r.pembery@bath.ac.uk>
2020-02-12 21:51:04 +0000

    Added script that downloads all the extra repos.

---
+++ download_auxilliary_files.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd ..
+
+git clone https://github.com/orpembery/helmholtz-firedrake.git
+
+git clone https://github.com/orpembery/helmholtz-nearby-preconditioning.git
+
+git clone https://github.com/orpembery/helmholtz-monte-carlo.git
+
+git clone https://github.com/orpembery/running-helmholtz-monte-carlo.git
+
+git clone https://github.com/orpembery/prob-gmres-examples.git
+
+git clone https://github.com/orpembery/running-nbpc.git
+
+# Need to do data
